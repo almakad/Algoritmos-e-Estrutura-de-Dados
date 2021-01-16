3ef109b93b9fa6a952275a3229051ba060ce59da
almakad <carls-f@live.com>
2021-01-16 18:41:34 -0300

    remove .vscode

--- Aula 4 - Lista Linear Sequencial/aula_4.js
+++ Aula 4 - Lista Linear Sequencial/aula_4.js
@@ -10,4 +10,25 @@ function buscaLista(l, busca) {
     }
 }
 
-buscaLista(lista, 7)
+(function (l, busca){
+    var i= 0
+    while(i < l.length && l[i] != busca) i++
+    if(i != l.length) console.log(`Resultado da pesquisa ${l[i]}, encontrado no índice ${i}`)
+})(lista, 5);
+
+
+function buscaListaSentinela(l, b) {
+    i = 0;
+    l[l.length] = b
+    while(l[i] != b){
+        i++
+    }
+    if(i != l.length){
+        console.log(l[i])
+        console.log(l)
+        return i
+    }
+}
+
+//buscaLista(lista, 7)
+//buscaListaSentinela(lista, 7)
